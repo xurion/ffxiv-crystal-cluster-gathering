cdfa7cc81cb4585526d030d41532703dd7a9ef6c
Dean James <dean.james6@tesco.com>
2024-11-23 09:55:55 +0000

    Add lightning

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { earth, fire } from "./data";
+import { earth, fire, lightning } from "./data";
 import { Entry } from "./Entry";
 
 export const App = () => {
@@ -8,15 +8,21 @@ export const App = () => {
       <h1>Crystal & Clustering Gathering</h1>
 
       <main className="flex">
-        <div className="w-1/2">
+        <div className="w-1/3">
           <div>Earth</div>
           {earth.map((entry, i) => (
             <Entry entry={entry} key={i} />
           ))}
         </div>
-        <div className="w-1/2">
+        <div className="w-1/3">
           <div>Fire</div>
           {fire.map((entry, i) => (
+            <Entry entry={entry} key={i} />
+          ))}
+        </div>
+        <div className="w-1/3">
+          <div>Lightning</div>
+          {lightning.map((entry, i) => (
             <Entry entry={entry} key={i} />
           ))}
         </div>
